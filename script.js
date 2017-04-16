23b3739e02560510cd38f084a568b4c1da56dd30
Nikki Byrne <nikkibyrne@Nikkis-MacBook-Pro.local>
2017-04-16 14:48:26 +0100

    es6 syntax added

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
-var me = 'LambdaSchool';
+const me = 'LambdaSchool';
+const you = 'Student';
+let numberOfCatsIOwn = 0;
 
-var you = 'Student';
-
-var numberOfCatsIOwn = 0;
-
-function buyCat() {
+const buyCat = () => {
   numberOfCatsIOwn++;
-  conosle.log('I now own ' + numberOfCatsIOwn + ' cats!');
+  console.log('I now own ' + numberOfCatsIOwn + ' cats!');
 }
+buyCat();
 
-var favoriteBooks = [
+
+const favoriteBooks = [
   'Captain Underpants',
   'Magic Treehouse',
   'Brown Bear, Brown Bear, What Do You See?',
   'Slaughterhouse 5',
 ];
 
-var likesCaptainUnderpants = function(bookList) {
-  var yes = false;
-  bookList.forEach(function(book) {
+const likesCaptainUnderpants = (bookList) => {
+  let yes = false;
+  bookList.forEach((book) => {
     if (book === 'Captain Underpants') yes = true;
   });
   return yes;
 }
 
-var result = likesCaptainUnderpants();
+const result = likesCaptainUnderpants(favoriteBooks);
+console.log(result);
 
-var sumInput = function() {
-  var total = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    total += arguments[i];
+
+const sumInput = (...args) => {
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
   }
   return total;
 }
 
-var sum = sumInput(1, 2, 3, 4, 5);
+const sum = sumInput(1, 2, 3, 4, 5);
+console.log(sum);
 
-var add = function(x, x) {
-  return x + x;
+const add = (x, y) => {
+  return x + y;
 }
+const total = add(5, 5);
+console.log(total);
 
-var sum = add(5, 5);
